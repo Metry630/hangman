29c7b593ebc23bcd3e157f577a2c4a075ac807db
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2022-06-16 20:22:57 +0800

    Use prettier.io

--- src/App.js
+++ src/App.js
@@ -1,62 +1,115 @@
-import { useState } from 'react'
-import React from 'react'
-import './App.css';
-import getRandomWord from './words.js'
-import { Spaces } from './components.js'
-import { MistakeCountDisplay } from './components.js';
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+import getRandomWord from "./words.js";
+import { Spaces } from "./components.js";
+import { MistakeCountDisplay } from "./components.js";
 
 let currentWord = getRandomWord();
-let splitCurrentWord= currentWord.split('')
-const firstHalfLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
-const secondHalfLetters = ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let splitCurrentWord = currentWord.split("");
+const firstHalfLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+];
+const secondHalfLetters = [
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 function App() {
   const [revealedLetters, setRevealedLetters] = useState([]);
   const [numMistakes, setNumMistakes] = useState(0);
-  
+
   const handleClick = (letter) => {
-    if(revealedLetters.includes(letter) === false){
-      setRevealedLetters([...revealedLetters, letter])
-      if(splitCurrentWord.includes(letter) === false){
-      setNumMistakes(n => n+1);
+    if (revealedLetters.includes(letter) === false) {
+      setRevealedLetters([...revealedLetters, letter]);
+      if (splitCurrentWord.includes(letter) === false) {
+        setNumMistakes((n) => n + 1);
       }
     }
-  }
-  function reload(){
+  };
+  function reload() {
     window.location.reload(false);
   }
-  return(
-    <div id ="hangman">
-    <div id="upper">
-      <div>
-        <button onClick = {reload} id = "reload">New game</button>
-        <div id = "spacesContainer">
-          <Spaces 
-          revealedLetters = {revealedLetters} 
-          splitCurrentWord = {splitCurrentWord}
-          />
+  return (
+    <div id="hangman">
+      <div id="upper">
+        <div>
+          <button onClick={reload} id="reload">
+            New game
+          </button>
+          <div id="spacesContainer">
+            <Spaces
+              revealedLetters={revealedLetters}
+              splitCurrentWord={splitCurrentWord}
+            />
+          </div>
+        </div>
+        <MistakeCountDisplay
+          numMistakes={numMistakes}
+          splitCurrentWord={splitCurrentWord}
+          revealedLetters={revealedLetters}
+        />
+      </div>
+      <div id="lower">
+        <div id="buttons1">
+          {firstHalfLetters.map((letter, idx) => (
+            <button
+              key={idx}
+              onClick={() => handleClick(letter)}
+              className={
+                !revealedLetters.includes(letter)
+                  ? "regular letter"
+                  : revealedLetters.includes(letter) &&
+                    splitCurrentWord.includes(letter)
+                  ? "correct letter"
+                  : "incorrect letter"
+              }
+            >
+              {letter}
+            </button>
+          ))}
+        </div>
+        <div id="buttons2">
+          {secondHalfLetters.map((letter) => (
+            <button
+              onClick={() => handleClick(letter)}
+              className={
+                !revealedLetters.includes(letter)
+                  ? "regular letter"
+                  : revealedLetters.includes(letter) &&
+                    splitCurrentWord.includes(letter)
+                  ? "correct letter"
+                  : "incorrect letter"
+              }
+            >
+              {letter}
+            </button>
+          ))}
         </div>
       </div>
-      <MistakeCountDisplay
-      numMistakes = {numMistakes} 
-      splitCurrentWord = {splitCurrentWord}
-      revealedLetters = {revealedLetters}
-      />
     </div>
-    <div id = "lower">
-      <div id = "buttons1">
-      {firstHalfLetters.map((letter, idx) => <button key = {idx} onClick={() => handleClick(letter)} 
-      className = {!revealedLetters.includes(letter) ? "regular letter" : 
-      revealedLetters.includes(letter) && splitCurrentWord.includes(letter) ? "correct letter" : 
-      "incorrect letter"}>{letter}</button>)}
-      </div>
-      <div id = "buttons2">
-      {secondHalfLetters.map((letter) => <button onClick={() => handleClick(letter)} 
-      className = {!revealedLetters.includes(letter) ? "regular letter" : 
-      revealedLetters.includes(letter) && splitCurrentWord.includes(letter) ? "correct letter" : 
-      "incorrect letter"}>{letter}</button>)}
-      </div>
-    </div>
-    </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/components.js
+++ src/components.js
@@ -1,58 +1,75 @@
-import hangman1 from './img/hangman1.jpeg'
-import hangman2 from './img/hangman2.jpeg'
-import hangman3 from './img/hangman3.jpeg'
-import hangman4 from './img/hangman4.jpeg'
-import hangman5 from './img/hangman5.jpeg'
-import hangman6 from './img/hangman6.jpeg'
-import hangman7 from './img/hangman7.jpeg'
-import hangman8 from './img/hangman8.jpeg'
-import hangman9 from './img/hangman9.jpeg'
-import hangman10 from './img/hangman10.jpeg'
-import win from './img/win.jpeg'
-export function Spaces({ splitCurrentWord, revealedLetters }){
+import hangman1 from "./img/hangman1.jpeg";
+import hangman2 from "./img/hangman2.jpeg";
+import hangman3 from "./img/hangman3.jpeg";
+import hangman4 from "./img/hangman4.jpeg";
+import hangman5 from "./img/hangman5.jpeg";
+import hangman6 from "./img/hangman6.jpeg";
+import hangman7 from "./img/hangman7.jpeg";
+import hangman8 from "./img/hangman8.jpeg";
+import hangman9 from "./img/hangman9.jpeg";
+import hangman10 from "./img/hangman10.jpeg";
+import win from "./img/win.jpeg";
+export function Spaces({ splitCurrentWord, revealedLetters }) {
   let i = 0;
-  return(
-    <ul id = "spaces">
-      {splitCurrentWord.map(function(letter, idx){
-    const letterDisplay = revealedLetters.includes(letter) ? letter : '_';
-    return <li key = {idx}>{letterDisplay}</li>
-  })}
+  return (
+    <ul id="spaces">
+      {splitCurrentWord.map(function (letter, idx) {
+        const letterDisplay = revealedLetters.includes(letter) ? letter : "_";
+        return <li key={idx}>{letterDisplay}</li>;
+      })}
     </ul>
-  )
+  );
 }
 
-export function MistakeCountDisplay({ revealedLetters, splitCurrentWord, numMistakes }){
-    if(splitCurrentWord.every(element => {
+export function MistakeCountDisplay({
+  revealedLetters,
+  splitCurrentWord,
+  numMistakes,
+}) {
+  if (
+    splitCurrentWord.every((element) => {
       return revealedLetters.includes(element);
-    })){
-      return(
-        <div id = "mistakes">
-          <img src={win}></img>
-          <h3>You Win!</h3>
-        </div>
-      )
-    }else if(numMistakes < 9){
-      return(
-        <div id = "mistakes">
-          <img src={
-            numMistakes === 0 ? hangman1 :
-            numMistakes === 1 ? hangman2 :
-            numMistakes === 2 ? hangman3 :
-            numMistakes === 3 ? hangman4 :
-            numMistakes === 4 ? hangman5 :
-            numMistakes === 5 ? hangman6 : 
-            numMistakes === 6 ? hangman7 :
-            numMistakes === 7 ? hangman8 : hangman9
-            }></img>
-          <h3>Mistakes left: {9 - numMistakes}</h3>
-        </div>
-      )
-    }else if(numMistakes >= 9){
-      return(
-        <div id = "mistakes">
-          <img src={hangman10}></img>
-          <h3>You lose! The word was {splitCurrentWord.join('')}</h3>
-        </div>
-      )
-    }
+    })
+  ) {
+    return (
+      <div id="mistakes">
+        <img src={win}></img>
+        <h3>You Win!</h3>
+      </div>
+    );
+  } else if (numMistakes < 9) {
+    return (
+      <div id="mistakes">
+        <img
+          src={
+            numMistakes === 0
+              ? hangman1
+              : numMistakes === 1
+              ? hangman2
+              : numMistakes === 2
+              ? hangman3
+              : numMistakes === 3
+              ? hangman4
+              : numMistakes === 4
+              ? hangman5
+              : numMistakes === 5
+              ? hangman6
+              : numMistakes === 6
+              ? hangman7
+              : numMistakes === 7
+              ? hangman8
+              : hangman9
+          }
+        ></img>
+        <h3>Mistakes left: {9 - numMistakes}</h3>
+      </div>
+    );
+  } else if (numMistakes >= 9) {
+    return (
+      <div id="mistakes">
+        <img src={hangman10}></img>
+        <h3>You lose! The word was {splitCurrentWord.join("")}</h3>
+      </div>
+    );
   }
+}
